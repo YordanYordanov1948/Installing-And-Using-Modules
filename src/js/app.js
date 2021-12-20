5a7b28d59c2ec0a260175711b996b440a444c037
Yordan <yordan_yordanov001@abv.bg>
2021-12-20 16:04:02 +0200

    ramda import

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import "../scss/app.scss";
 
-const R = require("ramda");
+import * as R from "ramda";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
