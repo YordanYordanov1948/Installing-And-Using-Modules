f5bd6ebb98e191e42af0f25bb8cef6711d75783d
Yordan <yordan_yordanov001@abv.bg>
2021-12-20 15:42:10 +0200

    ramda installed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import "../scss/app.scss";
 
+const R = require("ramda");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
